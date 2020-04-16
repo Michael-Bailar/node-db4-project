edaa14913fb13198bc93acd13815f2432d3cfa08
Michael-B1764 <mikebailar@gmail.com>
2020-04-16 15:07:52 -0700

    done with MVP

--- receipes/recipes-router.js
+++ receipes/recipes-router.js
@@ -14,4 +14,38 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: "Failed to get recipes" });
           });
 })
+
+router.get("/:id/shoppinglist", (req, res) => {
+    const { id } = req.params
+
+    Recipes.getShoppingList(id)
+    .then(list => {
+        if (list) {
+          res.json(list);
+        } else {
+          res.status(404).json({ message: 'Could not find list with the given id.' })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to get list', error: err });
+      });
+})
+
+router.get("/:id/instructions", (req, res) => {
+    const { id } = req.params
+
+    Recipes.getInstructions(id)
+    .then(list => {
+        if (list) {
+          res.json(list);
+        } else {
+          res.status(404).json({ message: 'Could not find list with the given id.' })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to get list', error: err });
+      });
+})
+
+
 module.exports = router;
